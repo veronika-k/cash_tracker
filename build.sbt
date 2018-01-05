af1d26edb59c6c475145c1da91a68a12e688b223
Ronny-K <veronikaselenelune@gmail.com>
2018-01-05 15:36:05 +0200

    model

--- build.sbt
+++ build.sbt
@@ -1,6 +1,23 @@
 name := "cash_tracker"
+val globalSettings = Seq[SettingsDefinition](
 
-version := "1.0"
+  version := "1.0",
 
-scalaVersion := "2.12.4"
-    
+  scalaVersion := "2.12.4"
+)
+
+val model = Project("model", file("model"))
+  .settings(globalSettings: _*)
+
+val repositories = Project("repositories", file("repositories"))
+  .dependsOn(model)
+  .settings(globalSettings: _*)
+  .settings(
+    libraryDependencies ++= Seq(
+      "com.typesafe.slick" %% "slick" % "3.2.1",
+      "org.slf4j" % "slf4j-nop" % "1.6.4",
+      "com.typesafe.slick" %% "slick-hikaricp" % "3.2.1",
+      "org.postgresql" % "postgresql" % "42.1.4",
+      "com.github.tminglei" %% "slick-pg" % "0.15.4"
+    )
+  )
--- model/src/main/scala/model.scala
+++ model/src/main/scala/model.scala
@@ -1,6 +1,32 @@
+import java.security.Timestamp
+
 /**
   * Created by inoquea on 05.01.18.
   */
-class User {
+
+package object model {
+
+  case class User(id: Int,
+                  login: String,
+                  password: String)
+
+  case class Wallet(id: Int,
+                    userId: Int,
+                    balance: BigDecimal)
+
+  case class Category(id: Int,
+                      name: String)
+
+  case class Expenditure(id: Long,
+                         walletId: Int,
+                         categoryId: Int,
+                         amount: BigDecimal,
+                         date: Timestamp
+                        )
+
+  case class Replenishment(id: Long,
+                           walletId: Int,
+                           amount: BigDecimal,
+                           date: Timestamp)
 
 }
