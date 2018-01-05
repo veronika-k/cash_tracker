1cc30cbb01afd10e98ea9021122230544a17efff
Ronny-K <veronikaselenelune@gmail.com>
2018-01-05 15:37:14 +0200

    model

--- model/src/main/scala/model.scala
+++ model/src/main/scala/model.scala
@@ -3,26 +3,19 @@ import java.security.Timestamp
 /**
   * Created by inoquea on 05.01.18.
   */
-
 package object model {
 
-  case class User(id: Int,
-                  login: String,
-                  password: String)
+  case class User(id: Int, login: String, password: String)
 
-  case class Wallet(id: Int,
-                    userId: Int,
-                    balance: BigDecimal)
+  case class Wallet(id: Int, userId: Int, balance: BigDecimal)
 
-  case class Category(id: Int,
-                      name: String)
+  case class Category(id: Int, name: String)
 
   case class Expenditure(id: Long,
                          walletId: Int,
                          categoryId: Int,
                          amount: BigDecimal,
-                         date: Timestamp
-                        )
+                         date: Timestamp)
 
   case class Replenishment(id: Long,
                            walletId: Int,
