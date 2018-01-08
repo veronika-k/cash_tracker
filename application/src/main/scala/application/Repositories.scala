6c49ec6faead4c42db4246da83f43aca40bc8a3b
Ronny-K <veronikaselenelune@gmail.com>
2018-01-08 19:37:26 +0200

    queries_2

--- application/src/main/scala/application/Repositories.scala
+++ application/src/main/scala/application/Repositories.scala
@@ -1,45 +1,98 @@
+import java.sql.Timestamp
+
 import slick.jdbc.PostgresProfile.api._
 
-object Repositories{
-  val db = Database.forURL("jdbc:postgresql://localhost:5432/airport?user=inoquea&password=11111111")
+object Repositories {
+  val db = Database.forURL(
+    "jdbc:postgresql://localhost:5432/airport?user=inoquea&password=11111111")
   val userRepo = new UserRepo(db)
   val walletRepo = new WalletRepo(db)
   val categoryRepo = new CategoryRepo(db)
   val transactionRepo = new TransactionRepo(db)
 
   def getUserWalletQuery(userId: Int) = {
-    userRepo.table.filter(_.id === userId)
-      .join(walletRepo.table).on{case (user, wallet) => user.id === wallet.userId}
+    userRepo.table
+      .filter(_.id === userId)
+      .join(walletRepo.table)
+      .on { case (user, wallet) => user.id === wallet.userId }
   }
 
-  def getUserTransactionsQuery(userId: Int) =
+  def getUserTransactionsQuery(userId: Int) = {
+    getUserWalletQuery(userId)
+      .join(transactionRepo.table)
+      .on {
+        case ((user, wallet), transaction) => wallet.id === transaction.walletId
+      }
+  }
+
+  def getUserCategoriesQuery(userId: Int) = {
+    userRepo.table
+      .filter(_.id === userId)
+      .join(categoryRepo.table)
+      .on { case (user, category) => user.id === category.userId }
+
+  }
+
+  def getUserCategoryQuery(userId: Int, categoryId: Int) = {
+    getUserCategoriesQuery(userId).filter {
+      case (user, category) => category.id === categoryId
+    }
+  }
+
+  def getUserCategoryTransactionsQuery(userId: Int, categoryId: Int) = {
+    getUserCategoryQuery(userId: Int, categoryId: Int)
+      .join(transactionRepo.table)
+      .on {
+        case ((user, category), transaction) =>
+          category.id === transaction.categoryId
+      }
+  }
+
+  def getUserWalletCategoryTransactionsQuery(userId: Int,
+                                             categoryId: Int,
+                                             walletId: Int) = {
+    getUserCategoryTransactionsQuery(userId: Int, categoryId: Int)
+      .join(walletRepo.table)
+      .on {
+        case (((user, category), transaction), wallet) =>
+          transaction.walletId === wallet.id
+      }
+  }
+
+  def getUserWalletsCategoriesTransactionsQuery(userId: Int) = {
+    getUserTransactionsQuery(userId)
+      .join(categoryRepo.table)
+      .on {
+        case (((user, wallet), transaction), category) =>
+          category.id === transaction.categoryId
+      }
+  }
+
+  //change Null
+  def getUserTransactionParams(userId: Int, categoryId: Int,
+                               walletId: Int, timeFrom:Timestamp, timeTo:Timestamp)={
+    getUserWalletsCategoriesTransactionsQuery(userId).filter{
+      case (((user, wallet), transaction), category) => if (categoryId != null) category.id === categoryId else true
+    }.filter{
+      case (((user, wallet), transaction), category) => if (walletId != null) wallet.id === walletId else true
+    }.filter{
+      case (((user, wallet), transaction), category) => if (timeFrom != null && timeTo != null)
+        (transaction.date < timeTo && transaction.date > timeFrom)
+      else true
+    }
+  }
+
+  def getUserCategoryPercents(userId: Int, walletId: Int, timeFrom:Timestamp, timeTo:Timestamp) =
   {
-    getUserWalletQuery(userId).join(transactionRepo.table)
-      .on{case ((user, wallet), transaction) => wallet.id === transaction.walletId }
+    val categoriesTransactionNumber = getUserTransactionParams(userId, null, walletId, timeFrom, timeTo)
+      .groupBy{case (((user, wallet), transaction), category) => (category.id, category.name)}
+      .map{case ((categoryId, categoryName), group) => (categoryId, categoryName, group.size)}
+    val allTransactionNumber = categoriesTransactionNumber.map(_._2).sum
+    val categoriesPercents = categoriesTransactionNumber.
+      map{
+      case (categoryId, categoryName, categorySize) =>
+        (categoryId, categoryName, categorySize/allTransactionNumber)
+    }
   }
 
-  def getUserCategoriesQuery(userId:Int) =
-  {
-    userRepo.table.filter(_.id === userId)
-      .join(categoryRepo.table).on{case (user, category) => user.id === category.userId}
-
-  }
-
-  def getUserCategoryQuery(userId:Int, categoryId: Int) =
-  {
-    getUserCategoriesQuery(userId).filter{case (user, category) => category.id === categoryId}
-  }
-
-  def getUserCategoryTransactionsQuery(userId:Int, categoryId: Int) =
-  {
-    getUserCategoryQuery(userId:Int, categoryId: Int).join(transactionRepo.table)
-      .on{case ((user, category), transaction) => category.id === transaction.categoryId }
-  }
-
-  def getUserWalletCategoryTransactionsQuery(userId:Int, categoryId: Int, walletId:Int)=
-  {
-    getUserCategoryTransactionsQuery(userId:Int, categoryId: Int)
-      .join(walletRepo.table).on{case (((user, category), transaction), wallet) =>
-      transaction.walletId === wallet.id}
-  }
 }
